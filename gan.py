fb9bc9f2f89eac274e1f8ad07bd89e0c2e48dd83
ahhCrap <32366265+ahhCrap@users.noreply.github.com>
2019-07-03 17:19:59 +0200

    Bug Fixes
    Fixes:
    - AutoencodingGAN neverending training.
    - tqdm Progress bar added.
    - New train function for AutoencodingGAN
    - Other small fixes

--- gan.py
+++ gan.py
@@ -15,6 +15,9 @@ import numpy as np
 import tensorflow as tf
 from tensorflow.python.framework.ops import disable_eager_execution
 
+from tqdm import tqdm
+
+
 
 disable_eager_execution()
 
@@ -28,17 +31,13 @@ def batches(x, batch_size):
     x = x[np.random.permutation(len(x))][:int(len(x)/batch_size)*batch_size]
     for i in range(0, len(x), batch_size):
         yield x[i:i+batch_size]
-
-        
         
 class DCGAN():
     def __init__(self, prior, img_shape=(28, 28, 1), load_from=None):
         self.img_shape = img_shape
         self.channels = img_shape[-1]
-        self.latent_dim = 100
+        self.latent_dim = prior.d
         self.prior = prior
-        
-          
 
         optimizer = Adam(0.0002, 0.5)
 
@@ -69,11 +68,12 @@ class DCGAN():
         
         self.G = self.generator
         self.D = self.discriminator
+        self.optimizer = optimizer
         
         
         if load_from:
-          self.generator.load_weights(load_from+"_g.h5")
-          self.discriminator.load_weights(load_from+"_d.h5")
+            self.generator.load_weights(load_from+"_g.h5")
+            self.discriminator.load_weights(load_from+"_d.h5")
 
     def build_generator(self):
 
@@ -125,8 +125,52 @@ class DCGAN():
 
         return Model(img, validity)
 
-    def train(self, X_train, epochs, batch_size=128, callbacks=[]):
+    def train_step(self, imgs, d_steps, g_steps, pruning):
+        batch_size = len(imgs)
 
+        # Adversarial ground truths
+        valid = np.ones((batch_size, 1))
+        fake = np.zeros((batch_size, 1))
+        
+        # Sample noise and generate a batch of new images
+        for _ in range(d_steps):
+            noise = self.prior(batch_size)
+            gen_imgs = self.generator.predict(noise)
+            # fakes_val = gen_imgs[:10]
+            
+            # ------------------------------- PRUNING --------------------------------
+            if pruning != 1:
+                gen_scores = self.discriminator.predict(gen_imgs)
+                gen_imgs_sorted_ids = tf.argsort(gen_scores,
+                                                 axis=0,
+                                                 direction='DESCENDING')
+                
+                leftover = int(gen_imgs_sorted_ids.shape[0]//(1/pruning))
+                fake_output_best_ids = gen_imgs_sorted_ids[:leftover, :]
+                
+                gen_imgs = tf.gather_nd(gen_imgs, fake_output_best_ids)
+                fake = np.zeros((gen_imgs.shape[0], 1))
+            # ------------------------------------------------------------------------
+
+            # Train the discriminator (real classified as ones and generated as zeros)
+            d_loss_real = self.discriminator.train_on_batch(imgs, valid)
+            
+            d_loss_fake = self.discriminator.train_on_batch(gen_imgs, fake)
+            
+            d_loss = 0.5 * np.add(d_loss_real, d_loss_fake)
+
+        #assert np.allclose(self.generator.predict(noise)[:10], fakes_val), "D update effects G"
+
+        #  Train Generator
+        g_loss = -1
+        for _ in range(g_steps):
+            # Train the generator (wants discriminator to mistake images as real)
+            y_val = self.discriminator.predict(imgs[:10])
+            g_loss = self.combined.train_on_batch(noise, valid)
+            #assert np.allclose(y_val, self.discriminator.predict(imgs[:10])), "G update effects D"
+    
+    def train(self, X_train, epochs, batch_size=128, callbacks=[], d_steps=1, g_steps=1, pruning=1):
+        print('Training DCGAN')
         X_train = np.reshape(X_train, [-1]+list(self.img_shape))
 
         # Adversarial ground truths
@@ -139,42 +183,52 @@ class DCGAN():
             #  Train Discriminator
             # ---------------------
 
-            # Select a random half of images
-            idx = np.random.randint(0, X_train.shape[0], batch_size)
-            imgs = X_train[idx]
             for batch_id, imgs in enumerate(batches(X_train, batch_size)):
-
-              # Sample noise and generate a batch of new images
-              noise = self.prior(batch_size)
-              gen_imgs = self.generator.predict(noise)
-              fakes_val = gen_imgs[:10]
-
-              # Train the discriminator (real classified as ones and generated as zeros)
-              d_loss_real = self.discriminator.train_on_batch(imgs, valid)
-              d_loss_fake = self.discriminator.train_on_batch(gen_imgs, fake)
-              d_loss = 0.5 * np.add(d_loss_real, d_loss_fake)
-
-              #assert np.allclose(self.generator.predict(noise)[:10], fakes_val), "D update effects G"
-
-              # ---------------------
-              #  Train Generator
-              # ---------------------
-
-              # Train the generator (wants discriminator to mistake images as real)
-              y_val = self.discriminator.predict(imgs[:10])
-              g_loss = self.combined.train_on_batch(noise, valid)
-              #assert np.allclose(y_val, self.discriminator.predict(imgs[:10])), "G update effects D"
-
-            # Plot the progress
-            print ("\n%d [D loss: %f, acc.: %.2f%%] [G loss: %f]" % (epoch, d_loss[0], 100*d_loss[1], g_loss))
+                self.train_step(imgs, d_steps, g_steps, pruning)
+                
             for callback, period in callbacks:
-              if (epoch+1) % period == 0:
-                callback()
+                if (epoch+1) % period == 0:
+                    callback()
     
     def save(self, file_prefix='dcgan'):
         self.generator.save_weights(f"{file_prefix}_g.h5")
         self.discriminator.save_weights(f"{file_prefix}_d.h5")
 
+
+class MiniGan(DCGAN):
+    def __init__(self, prior, img_shape=[2], load_from=None, neurons=512):
+        self.neurons = neurons
+        super().__init__(prior, img_shape, load_from)
+        
+    def build_generator(self):
+
+        model = Sequential()
+
+        model.add(Dense(self.neurons, activation="relu", input_dim=self.prior.d))
+        model.add(Dense(self.neurons, activation="relu", input_dim=self.prior.d))
+        model.add(Dense(self.neurons, activation="relu", input_dim=self.prior.d))
+        model.add(Dense(2, activation="tanh"))
+
+        noise = Input(shape=(self.prior.d,))
+        img = model(noise)
+
+        return Model(noise, img)
+
+    def build_discriminator(self):
+
+        model = Sequential()
+
+        model.add(Dense(self.neurons, activation="tanh", input_dim=self.prior.d))
+        model.add(Dense(self.neurons, activation="tanh", input_dim=self.prior.d))
+        model.add(Dense(self.neurons, activation="tanh", input_dim=self.prior.d))
+        
+        model.add(Dense(1, activation='sigmoid'))
+
+        img = Input(shape=self.img_shape)
+        validity = model(img)
+
+        return Model(img, validity)
+   
 
 class Uniform():
     def __init__(self, d=100):
@@ -210,22 +264,143 @@ class GradientInverser():
         self.iterate = K.function([g.inputs[0], reals], [loss, gradient])
 
     def invert(self, x):
-        z = gan.prior(1000)
-        fakes = gan.generator.predict(z).reshape([1000, -1])
+        z = self.gan.prior(1000)
+        fakes = self.gan.generator.predict(z).reshape([1000, -1])
         x_ = x.reshape([-1, fakes.shape[1]])
         z=z[cdist(fakes, x.reshape([-1, fakes.shape[1]])).argmin(0)]
         lr = z*0+0.1
         momentum = 0.5
         old_gradient = 0
         for i in range(40):
-            loss, gradient = iterate([z, x])
+            loss, gradient = self.iterate([z, x])
             gradient = (1-momentum)*gradient + momentum*old_gradient
             old_gradient = gradient
             loss = np.sum(loss, axis=(1, 2, 3))[...,None]
             new_z = z - lr*gradient
             new_z = np.clip(new_z, -1, 1)
-            new_loss, _ = iterate([new_z, x])
+            new_loss, _ = self.iterate([new_z, x])
             new_loss = np.sum(new_loss, axis=(1, 2, 3))[...,None]
             z = np.where(new_loss<loss, new_z, z+np.random.normal(0, 0.01, z.shape))
             lr = np.where(new_loss>loss, lr/2, lr*1.1)*0.9
         return z
+    
+    def __call__(self, x):
+        return self.invert(x)
+
+
+class AutoencodingGAN(DCGAN):
+    def __init__(self, prior, img_shape=(28, 28, 1), load_from=None, train_decoder=False):
+        super().__init__(prior, img_shape, load_from)
+
+        self.encoder = self.build_encoder()
+        x = Input(shape=(self.img_shape))
+        encoded = self.encoder(x)
+        if not train_decoder:
+            self.generator.trainable = False
+        decoded = self.generator(encoded)
+        self.autoencoder = Model(x, decoded)
+        self.autoencoder.compile(loss='mean_squared_error', optimizer=self.optimizer)
+
+
+        if load_from:
+            self.generator.load_weights(load_from+"_g.h5")
+            self.discriminator.load_weights(load_from+"_d.h5")
+            try:
+                self.encoder.load_weights(load_from+"_enc.h5")
+            except Exception as e:
+                print('WARNING: loaded the gan but the autoencoder is not loaded')
+                
+    def build_encoder(self):
+        # let's use the discriminator architecture
+        model = Sequential()
+
+        model.add(Conv2D(32, kernel_size=3, strides=2, input_shape=self.img_shape, padding="same"))
+        model.add(LeakyReLU(alpha=0.2))
+        model.add(Dropout(0.25))
+        model.add(Conv2D(64, kernel_size=3, strides=2, padding="same"))
+        model.add(ZeroPadding2D(padding=((0,1),(0,1))))
+        model.add(BatchNormalization(momentum=0.8))
+        model.add(LeakyReLU(alpha=0.2))
+        model.add(Dropout(0.25))
+        model.add(Conv2D(128, kernel_size=3, strides=2, padding="same"))
+        model.add(BatchNormalization(momentum=0.8))
+        model.add(LeakyReLU(alpha=0.2))
+        model.add(Dropout(0.25))
+        model.add(Conv2D(256, kernel_size=3, strides=1, padding="same"))
+        model.add(BatchNormalization(momentum=0.8))
+        model.add(LeakyReLU(alpha=0.2))
+        model.add(Dropout(0.25))
+        model.add(Flatten())
+        model.add(Dense(self.prior.d, activation='tanh')) 
+        # we assume that the prior is scaled from -1 to 1, otherwise 
+        # we would need to scale explicitly here
+
+        img = Input(shape=self.img_shape)
+        encoded = model(img)
+
+        return Model(img, encoded) 
+        
+        
+    def train(self, X_train, epochs, batch_size=128, callbacks=[], d_steps=1, g_steps=1, ae_steps=1):
+        print('Training AutoencodingGAN')
+        X_train = np.reshape(X_train, [-1]+list(self.img_shape))
+    
+        # Adversarial ground truths
+        valid = np.ones((batch_size, 1))
+        fake = np.zeros((batch_size, 1))
+    
+        for epoch in tqdm(range(epochs)):
+    
+            # ---------------------
+            #  Train Discriminator
+            # ---------------------
+    
+            for batch_id, imgs in enumerate(batches(X_train, batch_size)):
+                self.train_step(imgs, d_steps, g_steps, ae_steps=ae_steps)
+    
+            for callback, period in callbacks:
+                if (epoch+1) % period == 0:
+                    callback()
+            
+        
+    def train_step(self, imgs, d_steps, g_steps, ae_steps):
+        batch_size = len(imgs)
+
+        # Adversarial ground truths
+        valid = np.ones((batch_size, 1))
+        fake = np.zeros((batch_size, 1))
+        
+        ## Train the autoencoder
+        #ae_steps_done = 0
+        #while (ae_steps - ae_steps_done) > np.random.uniform(0, 1):
+        #    self.autoencoder.train_on_batch(imgs, imgs)
+        #    ae_steps_done += 1
+            
+        # Train the autoencoder 'ae_steps' times
+        for _ in range(ae_steps):
+            self.autoencoder.train_on_batch(imgs, imgs)
+        
+        # Sample noise and generate a batch of new images
+        for _ in range(d_steps):
+            noise = self.prior(batch_size)
+            gen_imgs = self.generator.predict(noise)
+
+            # Train the discriminator (real classified as ones and generated as zeros)
+            d_loss_real = self.discriminator.train_on_batch(imgs, valid)
+            d_loss_fake = self.discriminator.train_on_batch(gen_imgs, fake)
+            d_loss = 0.5 * np.add(d_loss_real, d_loss_fake)
+        
+        #  Train Generator
+        g_loss = -1
+        for _ in range(g_steps):
+            y_val = self.discriminator.predict(imgs[:10])
+            g_loss = self.combined.train_on_batch(noise, valid)
+            
+    def encode(self, x):
+        return self.encoder.predict(x)
+    
+    def generate(self, z):
+        return self.generator.predict(z)
+    
+    def discriminate(self, x):
+        return self.discriminator.predict(x)
